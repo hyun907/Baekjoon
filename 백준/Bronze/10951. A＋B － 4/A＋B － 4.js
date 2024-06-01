3d68ba63ae00c58c8ed97fb4b3207b2a6b954ed9
seunghyun <159671505+hyun907@users.noreply.github.com>
2024-06-01 19:00:18 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+// input = ['1 1', '2 3', '3 4', '9 8', '5 2']
+
+for (let i = 0; i < input.length; i++) {
+  let numbers = input[i].split(" ");
+  console.log(Number(numbers[0]) + Number(numbers[1]));
+}
