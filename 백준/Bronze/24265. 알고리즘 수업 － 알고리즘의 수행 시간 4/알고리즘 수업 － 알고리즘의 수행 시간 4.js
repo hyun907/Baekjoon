2531bbe4f2a5328db8cba4076497612e2232b41f
seunghyun <159671505+hyun907@users.noreply.github.com>
2025-02-10 22:53:29 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+const input = Number(fs.readFileSync(filePath).toString().trim());
+
+console.log((input * (input - 1)) / 2);
+console.log(2);
