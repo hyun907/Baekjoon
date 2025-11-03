b96f09f61358178163caa1cefd6a12eb386bfe14
seunghyun <159671505+hyun907@users.noreply.github.com>
2025-11-03 15:01:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 96 ms, Memory: 9708 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,24 +1,9 @@
 const fs = require("fs");
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const arr = input.map(Number);
 
-const sortAsc = function (numArr) {
-    const sortedArr = [];
-    
-    for (let i = 1; i < numArr.length; i++) {
-        let isInserted = false;
-        for (let j = 0; j < sortedArr.length; j++) {
-            if (numArr[i] < sortedArr[j]) {
-                sortedArr.splice(j, 0, numArr[i]);
-                isInserted = true;
-                break;
-            }
-        }
-        if (!isInserted) {
-            sortedArr.push(numArr[i]);
-        }
-    }
-    return sortedArr;
-}
+const n = Number(input[0]); 
+const arr = input.slice(1).map(Number);
 
-console.log(sortAsc(arr).join('\n'));
+arr.sort((a, b) => a - b);
+
+console.log(arr.join('\n'));
